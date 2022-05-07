f1be26fe6246b3e7c2b05b14235c76260c13cc5e
ryan831026 <ryan831026@gmail.com>
2022-05-06 22:22:35 -0400

    DONE!!!

--- quadrotor_backstepping.m
+++ quadrotor_backstepping.m
@@ -1,16 +1,19 @@
 clear; clc;
 
-T = 10;
+T = 20;
 S.m = 0.5;
 S.Ix = 0.1;
 S.Iy = 0.1;
-S.Iz = 0.1;
+S.Iz = 0.2;
 S.g = -9.8;
 
-x0 = [1 2 3 0.1 0.1 0.1 0.5 0.5 0.5 -0.4 -0.2 -0.2];
+x0 = [-0.3 -0.2 -0.1 0.1 0.2 0.3 -0.3 -0.2 -0.1 0.1 0.2 0.3];
 
 S.k1 = .1;
 S.k2 = 1;
+
+global Record
+Record = [];
 
 [ts, xs] = ode45(@(t,x) ode(t,x,S), [0 T], x0);
 
@@ -21,9 +24,19 @@ plot(ts,xs(:,1), ts,xs(:,2), ts,xs(:,3), ...
 legend({'x', 'y', 'z', 'a', 'b', 'c'}, 'Location', 'best')
 
 subplot(2,1,2);
-plot(ts,xs(:,7), ts,xs(:,8), ts,xs(:,9), ...
-     ts,xs(:,10), ts,xs(:,11), ts,xs(:,12))
-legend({'dx', 'dy', 'dz', 'p', 'q', 'r'}, 'Location', 'best')
+% plot(ts,xs(:,7), ts,xs(:,8), ts,xs(:,9), ...
+%      ts,xs(:,10), ts,xs(:,11), ts,xs(:,12))
+% legend({'dx', 'dy', 'dz', 'p', 'q', 'r'}, 'Location', 'best')
+
+
+for i = 1:3
+    plot(Record(i,:))
+    hold on;
+    plot(Record(i+6,:))
+end
+legend({'1d', '1', '2d', '2', '3d', '3'}, 'Location', 'best')
+
+
 
 function [x, y, z, a, b, c, dx, dy, dz, p, q, r] = state(s)
 
@@ -32,7 +45,8 @@ function [x, y, z, a, b, c, dx, dy, dz, p, q, r] = state(s)
 
 end
 
-function u = control(s, S, f, G, fa, Ga)
+%%%%%%%%%%%% t
+function u = control(t, s, S, f, G, fa, Ga)
 
     [x, y, z, a, b, c, dx, dy, dz, p, q, r] = state(s);
     xi = [dx; dy; dz; p; q; r];
@@ -41,6 +55,10 @@ function u = control(s, S, f, G, fa, Ga)
            -S.k1 * (a - c*sin(b));
            -S.k1 * (c*sin(a)*cos(b) + b*cos(a));
            -S.k1 * (c*cos(a)*cos(b) - b*sin(a))];
+    %%%%%%%%%%%%
+    global Record
+    Record = [Record [Phi; xi; t]];
+    %%%%%%%%%%%%
 
     Z = xi - Phi;
 
@@ -52,7 +70,10 @@ function u = control(s, S, f, G, fa, Ga)
     
     JV0 = [x y z a b c]';
 
-    u = pinv(Ga) * ( JPhi' * (f + G*xi) - fa - G'*JV0 - S.k2*Z );
+%     u = pinv(Ga) * ( JPhi' * (f + G*xi) - fa - G'*JV0 - S.k2*Z );
+    u = Ga \ ( JPhi' * (f + G*xi) - fa - G'*JV0 - S.k2*Z );
+
+    disp(Ga*pinv(Ga))
 
 end
 
@@ -79,7 +100,19 @@ function ds = ode(t, s, S)
     Ga(3,1) = -1/S.m * (cos(a) * cos(b));
     Ga(4,2) = 1/S.Ix; Ga(5,3) = 1/S.Iy; Ga(6,4) = 1/S.Iz;
     
-    u = control(s, S, f, G, fa, Ga);
+    %%%%% test dynamics
+    G = eye(6);
+    fa = zeros(6,1);
+    Ga = [[1; 2] zeros(2,3); eye(4)];
+    %%%%% test dynamics
+    
+    u = control(t, s, S, f, G, fa, Ga);
+
+    %%%%% test control
+    eta = [x; y; z; a; b; c];
+    xi = [dx; dy; dz; p; q; r];
+    u = pinv(Ga) * (-S.k2*xi + S.k1*S.k2*inv(G)*eta - fa - S.k1*inv(G)*eta);
+    %%%%% test control
 
     deta = f + G * xi;
     dxi = fa + Ga * u;
